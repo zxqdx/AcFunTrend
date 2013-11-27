e4fc3b62d598392c55b98bad4c2525b218e5b278
zxqdx <zhangx2@rose-hulman.edu>
2013-11-27 14:12:11 -0500

    new test

---
+++ src/test/ws.py
@@ -0,0 +1,10 @@
+from websocket import create_connection
+ws = create_connection("ws://59.175.227.54:88/ws")
+ws.send('{func:"auth","key":"3fe3f2b10bbad0a1","secret":"e0c64e4b2c6eba73a1bb2b5ba2a854bbc1fa592f"}')
+result =  ws.recv()
+print("Received '%s'" % result)
+ws.send('{func:"user.get",id:"1",requestId:"papaya1"}')
+ws.send('{func:"user.get",id:"2",requestId:"papaya2"}')
+result =  ws.recv()
+print("Received '{}'".format(result))
+ws.close()
