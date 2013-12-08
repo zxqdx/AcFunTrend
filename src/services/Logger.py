c2d7072405861d1f6d5dcc0ed9dd9f47f46ffe38
unknown <localmgr@CHENZ3-1.rose-hulman.edu>
2013-12-08 17:27:26 -0500

    Havent done anything after last tuesday

--- src/services/Logger.py
+++ src/services/Logger.py
@@ -3,25 +3,38 @@ Created on Nov 24, 2013
 
 @author: BigMoe
 '''
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-import gadget
+from miscellaneous import gadget
 import datetime
-
 class Logger(object):
     '''
     classdocs
+    物怪
     '''
 
 
-    def __init__(self, module):
+    def __init__(self, filename, module):
         '''
         Constructor
         '''
-        self.module = module
-        self.time = datetime.datetime.now()
+        self.module = None
         self.filename = None
+        self._open(filename, module)
         
-    def add(self, massage, level):
-        if isinstance(massage, Exception):
-            
-            gadget.write_file(self.filename, str(massage)+str(level))
+    def add(self, message, level):
+        if isinstance(message, Exception):
+            gadget.write_file(self.filename, str(datetime.datetime.now()) + "Meowu!")
+        else:
+            gadget.write_file(self.filename, str(datetime.datetime.now())+"---Messege: "+str(message)+"-----Level: "+str(level))
+
+    def _open(self,filename, module):
+        self.module = module
+        self.filename = filename
+        gadget.write_file(self.filename, "-----------Start at "+str(datetime.datetime.now())+"--------Module: "+str(self.module))
+
+    def close(self):
+        gadget.write_file(self.filename, "End at "+ str(datetime.datetime.now()))
+
+    
