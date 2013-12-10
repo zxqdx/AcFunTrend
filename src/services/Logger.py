46c48cf1fd1c6789299375ee9083ede1b654f12b
unknown <localmgr@CHENZ3-1.rose-hulman.edu>
2013-12-09 22:46:59 -0500

    haven't get much done again...

--- src/services/Logger.py
+++ src/services/Logger.py
@@ -8,6 +8,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from miscellaneous import gadget
 import datetime
+# import GLobal
 class Logger(object):
     '''
     classdocs
@@ -15,13 +16,13 @@ class Logger(object):
     '''
 
 
-    def __init__(self, filename, module):
+    def __init__(self, module):
         '''
         Constructor
         '''
         self.module = None
         self.filename = None
-        self._open(filename, module)
+        self._open(module)
         
     def add(self, message, level):
         if isinstance(message, Exception):
@@ -29,12 +30,13 @@ class Logger(object):
         else:
             gadget.write_file(self.filename, str(datetime.datetime.now())+"---Messege: "+str(message)+"-----Level: "+str(level))
 
-    def _open(self,filename, module):
+    def _open(self, module):
         self.module = module
-        self.filename = filename
+        self.filename = "{}/debug/{}/{}_{}.log".format("Glabal.wwwPath", str(datetime.datetime.now().year)+str(datetime.datetime.now().month), datetime.datetime.now().day, self.module)
         gadget.write_file(self.filename, "-----------Start at "+str(datetime.datetime.now())+"--------Module: "+str(self.module))
 
     def close(self):
         gadget.write_file(self.filename, "End at "+ str(datetime.datetime.now()))
 
     
+i = Logger("Ma")
