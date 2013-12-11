17c8ab086be573275376b9a4e8a7e4e79571f4d3
zxqdx <zhangx2@rose-hulman.edu>
2013-12-11 14:49:21 -0500

    Global.py

--- src/Global.py
+++ src/Global.py
@@ -1,1 +1,10 @@
-__author__ = 'zhangx2'
+__author__ = "zxqdx"
+
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from miscellaneous import gadget
+
+wwwPath = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+wwwPath = gadget.replace_all(wwwPath, "\\", "/")
